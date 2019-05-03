d198108825e95ea1fc979d1cf5a47fb9691c1d3b
3000A <49558692+3000A@users.noreply.github.com>
2019-05-03 09:01:43 +0800

    Update PinBall.java

--- PinBall.java
+++ PinBall.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+﻿import java.util.Random;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -163,7 +163,56 @@ public class PinBall
 				g.setColor(Color.red);
 				g.setFont(LifeFont);
 				g.drawString("Life:"+lives, 20,30);
+				//绘制砖块
+				map.draw((Graphics2D)g);
+
 			}
 		}
 	}
+
+	class MapGenerator {
+		public int brickWidth;
+		public int brickHeight;
+		public MapGenerator(int row,int col){
+			/*
+			 * s.length是针对java中数组的，length是数组的一个属性，用来表示数组的长度
+			   s.length()则是字符串的一个方法，用来返回字符串的长度的
+			      字符串是没有length属性的，只有length()方法
+			      数组有length属性，而没有length()方法 
+			*/
+			map = new int [row][col];
+			for(int i=0;i<map.length;i++){
+				for(int j = 0;j<map[0].length;j++){
+					map[i][j]=1;
+					//逻辑：遇到1就说明块还没有碰到球，遇到哪一块，那一块就置0
+				}
+			}
+			brickWidth=250/col;
+			brickHeight=80/row;
+		}
+			
+		public void draw(Graphics2D g){
+				for(int i=0;i<map.length;i++){
+					for(int j = 0;j<map[0].length;j++){
+						if(map[i][j]>0){
+							g.setColor(Color.white);
+							g.fillRect(j*brickWidth+80, i*brickHeight+50, brickWidth, brickHeight);				}
+						//逻辑：遇到1就说明块还没有碰到球，遇到哪一块，那一块就置0
+						/*填充矩形的原方法应该是：public void fillRect(int,int int,int );4个参数的意思分别为，起始X坐标，起始Y坐标，宽度，高度。*/
+						
+						//为这些白色的边框设置边界
+						g.setStroke(new BasicStroke(3));
+						g.setColor(Color.black);
+						g.drawRect(j*brickWidth+35, i*brickHeight+50, brickWidth, brickHeight);
+						/*
+						 * BasicStroke是JDK中提供的一个基本的画笔类,我们对他设置画笔的粗细，就可以在drawPanel上任意画出自己想要的图形了。
+						 * */
+					}
+				}
+			}
+		//下面为修改砖块的值，实现消块做准备
+		public void setBrickValue(int value,int row,int col){
+			map[row][col]=value;
+		}
+		}
 }
